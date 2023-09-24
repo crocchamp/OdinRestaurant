e31aab8b8e2295d8044cc394446f5dd8aa830036
Jack <106277670+crocchamp@users.noreply.github.com>
2023-09-23 20:21:12 -0500

    Move files out of dist for pages

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 // main.js
 
-import { homeContent } from "../home.js";
-import { menuContent } from "../menu.js";
+import { homeContent } from "./home.js";
+import { menuContent } from "./menu.js";
 
-function showTabContent(tabId, content) {
+// Wrap your code in a DOMContentLoaded event handler
+document.addEventListener('DOMContentLoaded', () => {
+  // This code will run when the HTML content is fully loaded
+
+  function showTabContent(tabId, content) {
     const tabs = document.getElementsByClassName('tab-content');
     for (const tab of tabs) {
       tab.style.display = 'none';
@@ -11,15 +15,15 @@ function showTabContent(tabId, content) {
     document.getElementById(tabId).innerHTML = content;
     document.getElementById(tabId).style.display = 'block';
   }
-  
+
   // Add click event listeners to tab buttons
   document.getElementById('homeButton').addEventListener('click', () => {
     showTabContent('homeTab', homeContent);
   });
-  
+
   document.getElementById('menuButton').addEventListener('click', () => {
     showTabContent('menuTab', menuContent);
   });
 
-
-  
+  showTabContent("homeTab", homeContent);
+});
